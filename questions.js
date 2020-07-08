19841de3feb0ef4a94eb57426c8fc3ea3b2b982e
Benjamin Young <64510379+Byoung23@users.noreply.github.com>
2020-07-07 20:33:50 -0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,33 @@
+var questionArray = [{
+
+    
+    
+    question: 'Where is the correct place to insert a JavaScript?',
+    choices: ['The <body> section', 'The <head> section', 'Both the <head> section and the <body> section are correct', 'Other', ],
+    answer: 'Both the <head> section and the <body> section are correct',
+
+},
+{
+    question: 'How do you create a function in Javascript?',
+    choices: ['function myFunction()', 'function = myFunction()', 'function:myFunction()', 'None of the above'],
+    answer: 'function myFunction()',
+},
+{
+    question: 'Which is a correct IF statement in JavaScript',
+    choices: ['if i == 5 then', 'if (i == 5)', 'if I = 5 then', 'If I = 5', ],
+    answer: 'if (i == 5)',
+},
+{
+    question: 'How do you write "Hello World" in an alert box?',
+    choices: ['alert("Hello World")', 'msgBox("Hello World")', 'alertBox("Hello World")', 'msg("Hello World")', ],
+    answer: 'alert("Hello World")',
+},
+{
+    question: 'How does a FOR loop start?',
+    choices: ['for (i,=5;i++)', 'for (i=0; i <=5;i++)', 'for (i =0; i <=5)', 'for i = 1 to 5', ],
+    answer: 'for (i=0; i <5;i++)',
+},
+
+
+];
+//Quesion source: https://www.w3schools.com/quiztest/quiztest.asp?qtest=JS
